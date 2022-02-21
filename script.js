ca74ea172c2c13938dc3182b29915a97a29ad567
Raul Garcia <punkvomit21@gmail.com>
2022-02-20 23:47:15 -0500

    add vanish function to unseencode logo edit welcome message

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
+
+
+//UnseenCode logo appear and disapear functions
+const navbarLogo = document.getElementById('navbar-logo');
+const unseenLogo = document.getElementsByClassName('navbar-logo');
+
+function vanishLogo() {
+  navbarLogo.id= "vanish";
+}
+
+function appearLogo() {
+  navbarLogo.id = unseenLogo;
+}
+//event listeners
+navbarLogo.addEventListener('mouseover', appearLogo);
+navbarLogo.addEventListener('click', vanishLogo);
+
+
